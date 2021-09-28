971f8b4a7f6013de327c6758f7b6de147027d3c7
pten4uk <pten4ik99@yandex.ru>
2021-09-28 19:05:34 +0300

    Change database to PostgreSQL

--- config/settings.py
+++ config/settings.py
@@ -82,9 +82,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    },
 }
 
 
